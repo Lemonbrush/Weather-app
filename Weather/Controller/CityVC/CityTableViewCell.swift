1d295b5ef40c0729f7377fc15a4898e94477f8a2
Lemonbrush <aa_rubtsov@mail.ru>
2021-04-12 13:02:12 +0300

    Add corner radius and shadow to weather cells

--- Weather/Controller/CityVC/CityTableViewCell.swift
+++ Weather/Controller/CityVC/CityTableViewCell.swift
@@ -12,10 +12,20 @@ class CityTableViewCell: UITableViewCell {
     @IBOutlet weak var degreeLabel: UILabel!
     @IBOutlet weak var TimeLabel: UILabel!
     @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherBackgroundView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        //Making proper round corners
+        weatherBackgroundView.layer.cornerCurve = CALayerCornerCurve.continuous
+        weatherBackgroundView.layer.cornerRadius = 15
+        
+        //Making shadow
+        weatherBackgroundView.layer.shadowColor = UIColor.black.cgColor
+        weatherBackgroundView.layer.shadowOpacity = 0.07
+        weatherBackgroundView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
