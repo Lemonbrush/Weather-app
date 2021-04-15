4e3bc47d54443ff9c41c7b55121ffbb9345b9a48
Lemonbrush <aa_rubtsov@mail.ru>
2021-04-15 11:40:08 +0300

    Add Zoom effect to the fade transition animation

--- Weather/Controller/CityVC/FadeTransitionAnimator.swift
+++ Weather/Controller/CityVC/FadeTransitionAnimator.swift
@@ -24,12 +24,14 @@ class FadeTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         transitionContext.containerView.addSubview(toViewController.view)
         
         toViewController.view.alpha = 0
+        toViewController.view.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
         
         let duration = self.transitionDuration(using: transitionContext)
         
         //Animation
         UIView.animate(withDuration: duration) {
             
+            toViewController.view.transform = CGAffineTransform.identity.scaledBy(x: 1.0, y: 1.0)
             toViewController.view.alpha = 1
             
         } completion: { _ in
