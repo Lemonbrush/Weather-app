79442c7c9f3b8c32108b1734a63bf2c74455dd49
Lemonbrush <aa_rubtsov@mail.ru>
2021-04-18 15:37:16 +0300

    Add SQLite data base with Cities and their codes

--- CityListDBCreator/CityListDBCreator/AppDelegate.swift
+++ CityListDBCreator/CityListDBCreator/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        print(paths[0])
+        
         return true
     }
 
--- Weather/Controller/AddCityViewController/AddCityViewController.swift
+++ Weather/Controller/AddCityViewController/AddCityViewController.swift
@@ -6,16 +6,35 @@
 //
 
 import UIKit
+import CoreData
 
 class AddCityViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
+    
+    var cities: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Setting up search bar appearance
         searchBar.searchTextField.backgroundColor = UIColor.white
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "City")
+        
+        do {
+            cities = try managedContext.fetch(fetchRequest)
+        } catch {
+            print(error)
+        }
+        
+        print(cities)
     }
     
     @IBAction func cancelButtonPressed(_ sender: Any) {
